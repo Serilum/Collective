e1ebd796a84ffde0ed71bdfa2e839d2334003e7e
Rick South <rick@natamus.com>
2023-12-20 12:54:26 +0100

    (v7.23) Updated Spawn A Mob code. [+1]

--- Common/src/main/java/com/natamus/collective/data/GlobalVariables.java
+++ Common/src/main/java/com/natamus/collective/data/GlobalVariables.java
@@ -16,11 +16,11 @@ import java.util.concurrent.CopyOnWriteArrayList;
 public class GlobalVariables {
 	public static Random random = new Random();
 	public static RandomSource randomSource = RandomSource.create();
-	
+
 	// Spawn A Mob variables
-	public static CopyOnWriteArrayList<EntityType<?>> activesams = new CopyOnWriteArrayList<EntityType<?>>();
-	public static CopyOnWriteArrayList<SAMObject> samobjects = new CopyOnWriteArrayList<SAMObject>();
-	
+	public static CopyOnWriteArrayList<EntityType<?>> activeSAMEntityTypes = new CopyOnWriteArrayList<EntityType<?>>();
+	public static CopyOnWriteArrayList<SAMObject> globalSAMs = new CopyOnWriteArrayList<SAMObject>();
+
 	// Villager names; 1: guard-villagers, 2-4: farlanders
 	public static List<String> moddedvillagers = new ArrayList<String>(Arrays.asList("Villager", "WanderingTrader", "Guard", "Farlander", "ElderFarlander", "Wanderer", "ThiefWanderingTrader", "PlagueDoctor", "GoblinTrader", "VeinGoblinTrader", "RedMerchant", "WanderingWizard", "WanderingWinemaker", "WanderingBaker", "Bowman", "Crossbowman", "Horseman", "Nomad", "Recruit", "RecruitShieldman", "Gatekeeper", "VillagerEntityMCA"));
 
@@ -34,20 +34,20 @@ public class GlobalVariables {
 
 	// Mob drops
 	public static HashMap<EntityType<?>, List<Item>> entitydrops = null;
-	
+
 	// URLS
 	public static String playerdataurl = "https://api.mojang.com/users/profiles/minecraft/";
 	public static String skindataurl = "https://sessionserver.mojang.com/session/minecraft/profile/";
-	
+
 	// HashMaps to generate
 	public static Map<Block, BlockEntityType<?>> blocksWithTileEntity = new HashMap<Block, BlockEntityType<?>>();
-	
+
 	public static void generateHashMaps() {
 		// FAB tile entities.
 		blocksWithTileEntity.put(Blocks.CAMPFIRE, BlockEntityType.CAMPFIRE);
 		blocksWithTileEntity.put(Blocks.OAK_SIGN, BlockEntityType.SIGN);
 	}
-		
+
 	// Block and item collections
 	public static List<Block> growblocks = Arrays.asList(Blocks.GRASS_BLOCK, Blocks.DIRT, Blocks.COARSE_DIRT, Blocks.GRAVEL, Blocks.MYCELIUM, Blocks.PODZOL, Blocks.SAND, Blocks.RED_SAND);
 	public static List<Block> stoneblocks = Arrays.asList(Blocks.COBBLESTONE, Blocks.MOSSY_COBBLESTONE, Blocks.STONE, Blocks.STONE_BRICKS, Blocks.CHISELED_STONE_BRICKS, Blocks.CRACKED_STONE_BRICKS, Blocks.SMOOTH_STONE, Blocks.GRAVEL, Blocks.ANDESITE, Blocks.POLISHED_ANDESITE, Blocks.DIORITE, Blocks.POLISHED_DIORITE, Blocks.GRANITE, Blocks.POLISHED_GRANITE, Blocks.SANDSTONE, Blocks.CHISELED_SANDSTONE, Blocks.CUT_SANDSTONE, Blocks.SMOOTH_SANDSTONE, Blocks.RED_SANDSTONE, Blocks.CHISELED_RED_SANDSTONE, Blocks.CUT_RED_SANDSTONE, Blocks.SMOOTH_RED_SANDSTONE, Blocks.END_STONE, Blocks.END_STONE_BRICKS, Blocks.NETHERRACK, Blocks.NETHER_BRICKS, Blocks.RED_NETHER_BRICKS);
