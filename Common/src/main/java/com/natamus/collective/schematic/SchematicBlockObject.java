fd34999cc98373127be9a52b4535fdfaff221c28
Rick South <rick@natamus.com>
2023-12-28 20:11:08 +0100

    (v7.27-7.28) Cleaned up Schematic code. [+1]

--- Common/src/main/java/com/natamus/collective/schematic/SchematicBlockObject.java
+++ Common/src/main/java/com/natamus/collective/schematic/SchematicBlockObject.java
@@ -4,24 +4,23 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class SchematicBlockObject {
-
 	private final BlockPos position;
 	private final BlockState state;
-	
+
 	public SchematicBlockObject(BlockPos position, BlockState state) {
 		this.position = position;
 		this.state = state;
 	}
-	
+
 	public BlockPos getPosition() {
 		return position;
 	}
-	
+
 	public BlockState getState() {
 		return state;
 	}
-	
-	public BlockPos getPositionWithOfsset(int x, int y, int z) {
+
+	public BlockPos getPositionWithOffset(int x, int y, int z) {
 		return new BlockPos(x + position.getX(), y + position.getY(), z + position.getZ());
 	}
 }
