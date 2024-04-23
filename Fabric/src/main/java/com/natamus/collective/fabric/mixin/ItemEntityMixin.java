7c57dd198e3beae3c78bfd5f0cebcf1b4bbef8be
Rick South <rick@natamus.com>
2024-04-23 17:42:45 +0200

    (v7.48-7.49) Updated ItemEntityMixin for Fabric to be able to edit itemstack data on pickup.

--- Fabric/src/main/java/com/natamus/collective/fabric/mixin/ItemEntityMixin.java
+++ Fabric/src/main/java/com/natamus/collective/fabric/mixin/ItemEntityMixin.java
@@ -2,17 +2,24 @@ package com.natamus.collective.fabric.mixin;
 
 import com.natamus.collective.fabric.callbacks.CollectiveItemEvents;
 import com.natamus.collective.fabric.callbacks.CollectivePlayerEvents;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = ItemEntity.class, priority = 1001)
-public abstract class ItemEntityMixin {
-	private ItemStack copyStack = null;
+public abstract class ItemEntityMixin extends Entity {
+	public ItemEntityMixin(EntityType<?> entityType, Level level) {
+		super(entityType, level);
+	}
 
 	@Inject(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/item/ItemEntity;discard()V", ordinal = 1))
 	public void ItemEntity_tick(CallbackInfo ci) {
@@ -21,13 +28,12 @@ public abstract class ItemEntityMixin {
 		CollectiveItemEvents.ON_ITEM_EXPIRE.invoker().onItemExpire(itemEntity, itemStack);
 	}
 
-	@Inject(method = "playerTouch(Lnet/minecraft/world/entity/player/Player;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;getCount()I"))
-	public void ItemEntity_playerTouch_Pre(Player player, CallbackInfo ci) {
-		copyStack = ((ItemEntity)(Object)this).getItem().copy();
-	}
+	@Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Inventory;add(Lnet/minecraft/world/item/ItemStack;)Z"), method = "playerTouch")
+	public boolean playerTouch(Inventory inventory, ItemStack itemStack) {
+		Player player = inventory.player;
 
-	@Inject(method = "playerTouch(Lnet/minecraft/world/entity/player/Player;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;take(Lnet/minecraft/world/entity/Entity;I)V"))
-	public void ItemEntity_playerTouch_Post(Player player, CallbackInfo ci) {
-		CollectivePlayerEvents.ON_ITEM_PICKED_UP.invoker().onItemPickedUp(player.level(), player, copyStack);
+		CollectivePlayerEvents.ON_ITEM_PICKED_UP.invoker().onItemPickedUp(player.level(), player, itemStack);
+
+		return inventory.add(itemStack);
 	}
 }
