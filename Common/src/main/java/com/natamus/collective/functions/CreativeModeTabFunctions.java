63a8264ae8596efeecf1e0fb158303252816a165
Rick South <rick@natamus.com>
2024-06-23 01:27:36 +0200

    (v7.64) Fixed isNumber function returning a false positive. [+1]

---
+++ Common/src/main/java/com/natamus/collective/functions/CreativeModeTabFunctions.java
@@ -0,0 +1,20 @@
+package com.natamus.collective.functions;
+
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
+
+import javax.annotation.Nullable;
+
+public class CreativeModeTabFunctions {
+	public static @Nullable ResourceKey<CreativeModeTab> getCreativeModeTabResourceKey(String path) {
+		return getCreativeModeTabResourceKey("minecraft", path);
+	}
+	public static @Nullable ResourceKey<CreativeModeTab> getCreativeModeTabResourceKey(String namespace, String path) {
+		return getCreativeModeTabResourceKey(new ResourceLocation(namespace, path));
+	}
+	public static @Nullable ResourceKey<CreativeModeTab> getCreativeModeTabResourceKey(ResourceLocation resourceLocation) {
+		return BuiltInRegistries.CREATIVE_MODE_TAB.getResourceKey(BuiltInRegistries.CREATIVE_MODE_TAB.get(resourceLocation)).orElseGet(() -> { return null; });
+	}
+}
