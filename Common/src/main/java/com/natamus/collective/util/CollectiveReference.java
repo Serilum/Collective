b9c82adc4893536cf751c44cf072679dc84a28f0
Rick South <rick@natamus.com>
2024-06-27 11:41:47 +0200

    (v7.65) Updated ToolFunctions to improve compatibility. [+1]

--- Common/src/main/java/com/natamus/collective/util/CollectiveReference.java
+++ Common/src/main/java/com/natamus/collective/util/CollectiveReference.java
@@ -3,6 +3,6 @@ package com.natamus.collective.util;
 public class CollectiveReference {
 	public static final String MOD_ID = "collective";
 	public static final String NAME = "Collective";
-	public static final String VERSION = "7.64";
+	public static final String VERSION = "7.65";
 	public static final String ACCEPTED_VERSIONS = "[1.20.6]";
 }
