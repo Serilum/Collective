98e556b04ee9740269a5ac2dc68740f91c63a53d
Rick South <rick@natamus.com>
2024-07-14 10:16:27 +0200

    (v7.71) Fixed a NullPointerException in TeleportHelper.

--- Common/src/main/java/com/natamus/collective/util/CollectiveReference.java
+++ Common/src/main/java/com/natamus/collective/util/CollectiveReference.java
@@ -3,6 +3,6 @@ package com.natamus.collective.util;
 public class CollectiveReference {
 	public static final String MOD_ID = "collective";
 	public static final String NAME = "Collective";
-	public static final String VERSION = "7.70";
+	public static final String VERSION = "7.71";
 	public static final String ACCEPTED_VERSIONS = "[1.20.1]";
 }
