529fc7651be8aa82932d88ff1c8bb1a6e7410fb6
Rick South <rick@natamus.com>
2024-07-27 15:30:27 +0200

    (v7.75) Updated config screen init code. [+1]

--- Fabric/src/main/java/com/natamus/collective/fabric/config/FabricCollectiveConfigScreen.java
+++ Fabric/src/main/java/com/natamus/collective/fabric/config/FabricCollectiveConfigScreen.java
@@ -5,7 +5,7 @@ import com.natamus.collective.util.CollectiveReference;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 
-public class CollectiveModMenu implements ModMenuApi {
+public class FabricCollectiveConfigScreen implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
         return parent -> DuskConfig.DuskConfigScreen.getScreen(parent, CollectiveReference.MOD_ID);
