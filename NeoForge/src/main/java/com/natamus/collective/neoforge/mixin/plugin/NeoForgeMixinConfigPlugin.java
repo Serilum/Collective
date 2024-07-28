22e77d10c0d8ab6e5727ea81a5d389b85ee5f2cb
Rick South <rick@natamus.com>
2024-07-28 20:01:16 +0200

    (v7.80) Reverted the changes made in Collective version 7.79. [+1]

--- NeoForge/src/main/java/com/natamus/collective/neoforge/mixin/plugin/NeoForgeMixinConfigPlugin.java
+++ NeoForge/src/main/java/com/natamus/collective/neoforge/mixin/plugin/NeoForgeMixinConfigPlugin.java
@@ -1,6 +1,7 @@
 package com.natamus.collective.neoforge.mixin.plugin;
 
-import com.natamus.collective.check.ShouldLoadCheck;
+import com.natamus.collective.bundle.BundleConfigCheck;
+import com.natamus.collective.neoforge.bundle.NeoForgeBundleJarJarCheck;
 import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
@@ -34,7 +35,11 @@ public class NeoForgeMixinConfigPlugin implements IMixinConfigPlugin {
 
 		String modId = pSpl[2].split("_")[0];
 
-		return ShouldLoadCheck.shouldLoad(modId);
+        if (NeoForgeBundleJarJarCheck.isModJarJard(modId)) {
+            return BundleConfigCheck.isBundleModEnabled(modId);
+        }
+
+        return true;
 	}
 
     @Override
