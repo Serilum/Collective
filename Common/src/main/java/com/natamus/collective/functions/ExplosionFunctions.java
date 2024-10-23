44f9be8d9158dcda4428cab0a4bb3d2346210a7f
Rick South <rick@natamus.com>
2024-10-24 00:52:59 +0200

    (v7.87) Further extended ExplosionFunctions, to aid in updating dependent mods.

--- Common/src/main/java/com/natamus/collective/functions/ExplosionFunctions.java
+++ Common/src/main/java/com/natamus/collective/functions/ExplosionFunctions.java
@@ -9,4 +9,9 @@ public class ExplosionFunctions {
 	public static List<BlockPos> getAffectedBlockPositions(Explosion explosion) {
 		return explosion.getToBlow();
 	}
+
+	public static void clearExplosion(Explosion explosion) {
+		explosion.getToBlow().clear();
+		explosion.getHitPlayers().clear();
+	}
 }
