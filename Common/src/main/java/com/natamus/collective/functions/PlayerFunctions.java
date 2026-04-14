22a8bcca19e1cdb7f8285b1fa4e660659ea55162
Rick South <rick@natamus.com>
2026-04-14 12:30:17 +0200

    (v8.20) Updated PlayerFunctions to fix some respawn related issues.

--- Common/src/main/java/com/natamus/collective/functions/PlayerFunctions.java
+++ Common/src/main/java/com/natamus/collective/functions/PlayerFunctions.java
@@ -30,12 +30,11 @@ import java.util.*;
 
 public class PlayerFunctions {
 	public static boolean respawnPlayer(Level world, Player player) {
-		if (!(player instanceof ServerPlayer)) {
+		if (!(player instanceof ServerPlayer serverplayer)) {
 			return false;
 		}
 
 		MinecraftServer server = world.getServer();
-		ServerPlayer serverplayer = (ServerPlayer)player;
 
 		if (serverplayer.wonGame) {
 			serverplayer.wonGame = false;
@@ -43,7 +42,10 @@ public class PlayerFunctions {
 			CriteriaTriggers.CHANGED_DIMENSION.trigger(serverplayer, Level.END, Level.OVERWORLD);
 		}
 		else if (serverplayer.getHealth() <= 0.0F) {
-			 server.getPlayerList().respawn(serverplayer, false, Entity.RemovalReason.KILLED);
+			server.getPlayerList().respawn(serverplayer, false, Entity.RemovalReason.KILLED);
+		}
+		else if (serverplayer.isSpectator()) {
+			server.getPlayerList().respawn(serverplayer, false, Entity.RemovalReason.KILLED);
 		}
 
 		return true;
