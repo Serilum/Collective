d4f816ff40b1f4d1a002a5d789fe96fa398ad643
Rick South <rick@natamus.com>
2026-06-24 10:54:03 +0200

    (v8.31) Added 'updateMinecraftWindowTitleInDevMode' config, disabled by default and only when ran from an IDE. [+1]

---
+++ NeoForge/src/main/java/com/natamus/collective/neoforge/mixin/MinecraftMixin.java
@@ -0,0 +1,19 @@
+package com.natamus.collective.neoforge.mixin;
+
+import com.natamus.collective.config.CollectiveConfigHandler;
+import com.natamus.collective.services.Services;
+import net.minecraft.client.Minecraft;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = Minecraft.class, priority = 1001)
+public class MinecraftMixin {
+	@Inject(method = "createTitle", at = @At("RETURN"), cancellable = true)
+	private void Minecraft_createTitle(CallbackInfoReturnable<String> cir) {
+        if (CollectiveConfigHandler.updateMinecraftWindowTitleInDevMode && Services.MODLOADER.isDevelopmentEnvironment()) {
+            cir.setReturnValue("Minecraft · Dev mode · " + Services.MODLOADER.getModLoaderName());
+        }
+	}
+}
